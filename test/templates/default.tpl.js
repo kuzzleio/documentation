c162da1dcd8c2efa52e5a42bad7e27434d33f442
njuelle <n.juelle@gmail.com>
2018-06-14 17:27:48 +0200

    test real code example in JS

--- test/templates/default.tpl.js
+++ test/templates/default.tpl.js
@@ -1,1 +1,23 @@
-[snippet-code]
+
+// load the Kuzzle SDK module
+const Kuzzle = require('kuzzle-sdk')
+
+// instantiate a Kuzzle client, this will automatically connect to the Kuzzle server
+const kuzzle = new Kuzzle('localhost', { defaultIndex: 'playground' })
+
+// add a listener to detect any connection problems
+kuzzle.on("networkError", function (error) {
+  console.error("Network Error:" + error);
+})
+
+// create a 'playground' index and then a collection named 'mycollection' that we can use to store data
+kuzzle
+  .createIndexPromise('playground')
+  .then(() => kuzzle.collection('mycollection').createPromise())
+  .then(() => {
+    [snippet-code]
+  })
+  .catch(err => {
+    console.error(err.message)
+  })
+  .finally(() => kuzzle.disconnect())
