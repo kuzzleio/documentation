27f9e70505bcd38b4381bcd675ea846002b5dd2c
njuelle <n.juelle@gmail.com>
2018-06-15 12:02:40 +0200

    fix JS entrypoint

--- docker/entrypoints/js.sh
+++ docker/entrypoints/js.sh
@@ -3,5 +3,5 @@
 set -e
 
 npm install
-npm i -g eslint
+npm i -g eslint eslint-plugin-import@latest eslint-plugin-node@latest eslint-plugin-promise@latest eslint-plugin-standard@latest
 node test/docTester.js -L js
