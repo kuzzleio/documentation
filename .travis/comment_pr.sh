6c730a49eb58403feb20cbd946aa37d243ccf8d4
njuelle <n.juelle@gmail.com>
2018-06-20 14:49:22 +0200

    send comment to PR

---
+++ .travis/comment_pr.sh
@@ -0,0 +1,6 @@
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
+  COMMENT="#TEST REPORT\n GO:https://docs-v2.kuzzle.io/reports/go/${TRAVIS_COMMIT}\n JS:https://docs-v2.kuzzle.io/reports/js/${TRAVIS_COMMIT}\n"
+  curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"$COMMENT\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
