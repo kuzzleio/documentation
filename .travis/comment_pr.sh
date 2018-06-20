6cd30e2f9f885db2d5febf55a5e6b4e632d24d6d
njuelle <n.juelle@gmail.com>
2018-06-20 15:29:28 +0200

    echo

--- .travis/comment_pr.sh
+++ .travis/comment_pr.sh
@@ -3,6 +3,10 @@ set -e
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
   COMMENT="#TEST REPORT\n GO:https://docs-v2.kuzzle.io/reports/go/${TRAVIS_COMMIT}\n JS:https://docs-v2.kuzzle.io/reports/js/${TRAVIS_COMMIT}\n"
+  echo 'curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    -d "{\"body\": \"$COMMENT\"}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments'
+
   curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"$COMMENT\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
