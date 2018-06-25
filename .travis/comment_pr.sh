97680ef753f6e4bb63d3809f750e4f8ad87abc27
njuelle <n.juelle@gmail.com>
2018-06-25 10:29:51 +0200

    perms on kuttlefish

--- .travis/comment_pr.sh
+++ .travis/comment_pr.sh
@@ -2,4 +2,5 @@
 set -e
 
 wget https://github.com/alexandrebouthinon/kuttlefish/releases/download/v1/kuttlefish
+chmod +x kuttlefish
 ./kuttlefish -f ./comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -type 0
