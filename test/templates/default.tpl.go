2d05e64f40e60dcd38ff4ac50c59a2a1c94ff19d
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2018-06-25 18:31:50 +0200

    Fix template

--- test/templates/default.tpl.go
+++ test/templates/default.tpl.go
@@ -3,22 +3,22 @@ package main
 import "fmt"
 
 func main() {
-    conn := websocket.NewWebSocket("kuzzle:7512", nil)
+    conn := websocket.NewWebSocket("kuzzle", nil)
     k, _ := kuzzle.NewKuzzle(conn, nil)
-    
+
     err := k.Connect()
-    
+
     if err != nil {
         fmt.Println(err.Error())
         return
     }
-    
+
     _, err = k.Server.GetAllStats(nil)
 
     if err != nil {
         fmt.Println(err.Error())
         return
     }
-    
+
     [snippet-code]
 }
