ee3acd2c15a79e2796e2ce650a7f01d078d6ebf2
njuelle <n.juelle@gmail.com>
2018-06-25 13:25:26 +0200

    dqfgds

--- .travis/comment_pr.sh
+++ .travis/comment_pr.sh
@@ -4,6 +4,6 @@ set -e
 cd .travis
 wget https://github.com/alexandrebouthinon/kuttlefish/releases/download/v1/kuttlefish
 ls -la
-chmod +x kuttlefish
-chmod +rw comment.html
-./kuttlefish -f comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -t 0
+chmod +x ./kuttlefish
+chmod +rw ./comment.html
+./kuttlefish -f ./comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -t 0
