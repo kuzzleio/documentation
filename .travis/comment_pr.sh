86b476daa63bfa053dd1eb1da916319fac473742
njuelle <n.juelle@gmail.com>
2018-06-25 10:20:40 +0200

    fix again 2

--- .travis/comment_pr.sh
+++ .travis/comment_pr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-../../kuttlefish -f ./comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -type 0
+../kuttlefish -f ./comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -type 0
