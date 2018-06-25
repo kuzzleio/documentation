5e56481517ade1fd16db082e912c8044f353e0c1
njuelle <n.juelle@gmail.com>
2018-06-25 11:26:05 +0200

    sfqfff

--- .travis/comment_pr.sh
+++ .travis/comment_pr.sh
@@ -3,4 +3,5 @@ set -e
 
 wget https://github.com/alexandrebouthinon/kuttlefish/releases/download/v1/kuttlefish
 chmod +x kuttlefish
-./kuttlefish -f .travis/comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -t 0
+chmod 777 comment.html
+./kuttlefish -f ./comment.html -id $TRAVIS_PULL_REQUEST -r $TRAVIS_REPO_SLUG -token $GH_TOKEN -t 0
