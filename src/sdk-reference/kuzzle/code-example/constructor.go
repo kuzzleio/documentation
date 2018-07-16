c6627618b793a69326ef997dec182ad987b62d70
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-07-16 11:59:21 +0200

    Fix go constructor snippet

--- src/sdk-reference/kuzzle/code-example/constructor.go
+++ src/sdk-reference/kuzzle/code-example/constructor.go
@@ -1,7 +1,7 @@
-cOpts := types.NewOptions()
-cOpts.setPort(7512)
-c := websocket.NewWebSocket("kuzzle", cOpts)
+opts := types.NewOptions()
+opts.SetPort(7512)
+c := websocket.NewWebSocket("kuzzle", opts)
 
-kOpts := types.NewOptions()
-kOpts.SetAutoResubscribe(false)
-kuzzle, err := kuzzle.NewKuzzle(c, kOpts)
+o := types.NewOptions()
+o.SetAutoResubscribe(false)
+k, _ := kuzzle.NewKuzzle(c, o)
--- test/templates/blank.tpl.go
+++ test/templates/blank.tpl.go
@@ -5,8 +5,11 @@ import (
 
 	"github.com/kuzzleio/sdk-go/connection/websocket"
 	"github.com/kuzzleio/sdk-go/kuzzle"
+	"github.com/kuzzleio/sdk-go/types"
 )
 
 func main() {
 	[snippet-code]
+	k.Server.Now(nil)
+	fmt.Println("Everything is ok")
 }
