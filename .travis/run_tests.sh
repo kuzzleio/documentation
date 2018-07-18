236f4f096f3e0fcc3fbf190e063fb66a8ee82358
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-07-18 15:24:54 +0200

    use TRAVIS var

--- .travis/run_tests.sh
+++ .travis/run_tests.sh
@@ -13,7 +13,7 @@ for language in js go cpp; do
 
   sh run_test.sh -l $language -n
 
-  if [ ! -z "$TRAVIS_PULL_REQUEST" ]; then
+  if [ ! -z "$TRAVIS" ]; then
     aws s3 cp reports/ s3://$AWS_S3_BUCKET/reports/$language/$TRAVIS_PULL_REQUEST/ --recursive --exclude "*.gitkeep"
   fi
 
