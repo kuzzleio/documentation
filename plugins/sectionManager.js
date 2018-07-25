0d9490d8ecbd8dcccac8e97a05c975106617b379
njuelle <n.juelle@gmail.com>
2018-07-25 11:04:30 +0200

    fix md renderer + bulk import new model

--- plugins/sectionManager.js
+++ plugins/sectionManager.js
@@ -1,8 +1,8 @@
 
 const fs = require('fs');
 const path = require('path');
-const marked = require('marked');
 const config = require('../getConfig').get();
+const markdownIt = require('markdown-it');
 
 module.exports = {
   
@@ -30,19 +30,20 @@ module.exports = {
         name = el.split('=')[1].slice(0, -1),
         fullPath = path.join(__dirname, `../src/${filename.split('/').slice(0, -1).join('/')}`),
         filenames = fs.readdirSync(fullPath),
-        languages = config.languages;
+        languages = config.languages,
+        md = new markdownIt();
       
       let section = '';
         
-      languages['default'] = {fullname:'default'};
+      languages.default = {fullname:'default'};
       
-      filenames.forEach(function (file) {
+      filenames.forEach(file => {
         if (file.split('.')[0] === name) {
           const 
             language = file.split('.')[1],
             fileContent = fs.readFileSync(fullPath + '/' + file, 'utf8');
           
-          section += '<div id="' + name + '" class="section ' + languages[language].fullname + '">\n' + marked(fileContent) + '\n </div>';
+          section += `<div id="${name}" class="section ${languages[language].fullname}">\n${md.render(fileContent)}\n </div>`;
         }
       });
       
@@ -54,13 +55,10 @@ module.exports = {
     const reMatch = fileString.match(/(\[section=)[a-zA-Z0-9]+\]/g); 
     
     if (reMatch) {
-      fileString = this.injectSection(reMatch, filename, fileString)
+      fileString = this.injectSection(reMatch, filename, fileString);
     }
     
     return fileString;
   }
 
-} 
-
-
-
+}; 
