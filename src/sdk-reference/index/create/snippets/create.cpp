4b5ced40e1ce7763e228fad1f014788461934959
njuelle <n.juelle@gmail.com>
2018-07-27 12:50:26 +0200

    fix create cpp test

--- src/sdk-reference/index/create/snippets/create.cpp
+++ src/sdk-reference/index/create/snippets/create.cpp
@@ -1,5 +1,5 @@
 try {
-  kuzzle->index->create("nyc-open-data");*
+  kuzzle->index->create("nyc-open-data");
   std::cout << "index created" << std::endl;
 } catch (kuzzleio::KuzzleException e) {
   std::cerr << e.getMessage() << std::endl;
