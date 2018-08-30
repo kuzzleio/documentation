7c2256253404f2549cb687ba90bf5f6d4c95bd5b
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-08-30 09:49:47 +0200

    Add missing template

---
+++ test/templates/without-connect.tpl.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "auth.hpp"
+#include "collection.hpp"
+#include "document.hpp"
+#include "index.hpp"
+#include "realtime.hpp"
+#include "kuzzle.hpp"
+#include "listeners.hpp"
+
+class MyListener : public kuzzleio::EventListener {
+  public:
+    void trigger(char *payload) const {
+      std::cout << payload << std::endl;
+    }
+};
+
+int main() {
+  std::string hostname = "kuzzle";
+
+  kuzzleio::options kuzzle_options = KUZZLE_OPTIONS_DEFAULT;
+
+  kuzzleio::Kuzzle* kuzzle = new kuzzleio::Kuzzle(hostname, &kuzzle_options);
+
+  [snippet-code]
+
+  std::cout << "Success" << std::endl;
+
+  return 0;
+}
