b5c79c2600c195ec5c8dca6d43e58c3a208e1ce3
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-09-13 11:01:37 +0200

    update template for async

--- src/sdk-reference/js/6/collection/exists/snippets/exists.js
+++
@@ -1,9 +0,0 @@
-try {
-  const exists = await kuzzle.collection.exists('nyc-open-data', 'green-taxi');
-
-  if (exists) {
-    console.log('Success');
-  }
-} catch (error) {
-  console.error(error.message);
-}
