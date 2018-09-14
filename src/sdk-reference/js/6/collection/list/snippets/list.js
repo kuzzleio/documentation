0f3ddeae78f98a83f65cae827d2dcdb8702d23d7
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-09-14 09:21:52 +0200

    add collection list

---
+++ src/sdk-reference/js/6/collection/list/snippets/list.js
@@ -0,0 +1,16 @@
+try {
+  const options = { from: 1, size: 1 };
+
+  const collectionList = await kuzzle.collection.list('mtp-open-data', options);
+  /*
+    {
+      type: 'all',
+      collections: [ { name: 'pink-taxi', type: 'stored' } ],
+      from: 1,
+      size: 1
+    }
+  */
+  console.log('Success');
+} catch (error) {
+  console.error(error.message);
+}
