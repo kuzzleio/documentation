549ae27adce4b3824f644e2213e7eb2aa66b2891
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-09-27 15:48:00 +0200

    Update validateSpecification return

--- src/sdk-reference/java/1/collection/validate-specifications/snippets/validate-specifications.java
+++ src/sdk-reference/java/1/collection/validate-specifications/snippets/validate-specifications.java
@@ -1,6 +1,8 @@
 try {
     String specifications = "{ \"nyc-open-data\": { \"yellow-taxi\": { \"strict\": false, \"fields\": { \"license\": { \"mandatory\": true, \"type\": \"string\" } } } } }";
-    if (kuzzle.getCollection().validateSpecifications(specifications)) {      
+    ValidationResponse validationResponse = kuzzle.getCollection().validateSpecifications(specifications);
+
+    if (validationResponse.getValid()) {
       System.out.println("Success");
     }
 } catch (KuzzleException e) {
