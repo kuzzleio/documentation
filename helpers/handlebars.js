cf72798b2341da7873a154cb75651c3c66fdc3b1
Sébastien Cottinet <seb.cottinet@gmail.com>
2018-10-02 14:10:32 +0200

    Add the possibility to exclude directories from the build (#41)
    * extend eslint to all js files + lint errors fixes
    
    * add an exclude directory option
    
    * make excluded directories available in dev mode
    
    * properly change the title format in templates
    
    * use <div> instead of <p>
    
    * fix ignored templates

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,29 +1,33 @@
-const handelbarsHelpers = {
+const
+  minimatch = require('minimatch'),
+  { SafeString } = require('handlebars');
+
+module.exports = {
   not: v => !v,
 
   eq: (v1, v2) => v1 === v2,
 
   ne: (v1, v2) => v1 !== v2,
-  
+
   lt: (v1, v2) => v1 < v2,
-  
+
   gt: (v1, v2) => v1 > v2,
-  
+
   lte: (v1, v2) => v1 <= v2,
-  
+
   gte: (v1, v2) => v1 >= v2,
-  
+
   and: (v1, v2) => v1 && v2,
-  
+
   or: (v1, v2) => v1 || v2,
-  
+
   startwith: (str, substr) => {
     if (!str) {
       return false;
     }
     return str.startsWith(substr);
   },
-  
+
   mstartwith: (str, ...args) => {
     if (!str) {
       return false;
@@ -31,11 +35,11 @@ const handelbarsHelpers = {
 
     return args.some(arg => str.startsWith(arg));
   },
-  
+
   endswith: (str, substr) => {
     return str.endsWith(substr);
   },
-  
+
   firstDefinedOf: (...args) => {
     return args.find(a => a);
   },
@@ -43,13 +47,22 @@ const handelbarsHelpers = {
   currentYear: () => {
     return new Date().getFullYear();
   },
-  
+
   dateToISO: (d) => {
     if (d instanceof Date) {
       return d.toISOString();
     }
     return d;
+  },
+
+  displayTitle: (data, options) => {
+    let title = data.title;
+    const config = options.data.root;
+
+    if (config.is_dev && config.exclude.some(e => minimatch(data.src, `**/${e}/**`))) {
+      title = `<div style='color:red'>(!) ${title}</div>`;
+    }
+
+    return new SafeString(title);
   }
 };
-
-module.exports = handelbarsHelpers;
