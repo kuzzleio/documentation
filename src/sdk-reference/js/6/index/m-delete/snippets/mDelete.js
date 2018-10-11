bbea7797e23e713114868a2f6656c66523e06a85
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2018-10-11 17:02:25 +0200

    [tests] Fix SDKs tests

--- src/sdk-reference/js/6/index/m-delete/snippets/mDelete.js
+++ src/sdk-reference/js/6/index/m-delete/snippets/mDelete.js
@@ -1,7 +1,7 @@
 kuzzle.index
   .mDelete(['nyc-open-data', 'mtp-open-data'])
   .then(response => {
-    console.log(`Successfully deleted ${response.deleted.length} indexes`);
+    console.log(`Successfully deleted ${response.length} indexes`);
   })
   .catch(error => {
     console.error(error.message);
