c0863b292b894f644972339335b3d6027593ebc9
jenow <jenova.ke20@gmail.com>
2018-10-12 16:13:18 +0200

    fix test

--- src/sdk-reference/java/1/auth/get-my-rights/snippets/get-my-rights.java
+++ src/sdk-reference/java/1/auth/get-my-rights/snippets/get-my-rights.java
@@ -1,6 +1,6 @@
 try {
     kuzzle.getAuth().login("local", "{\"username\":\"foo\",\"password\":\"bar\"}");
-    user_right rights = kuzzle.getAuth().getMyRights();
+    UserRight rights = kuzzle.getAuth().getMyRights();
     System.out.println("Success");
 } catch (KuzzleException e) {
     System.err.println(e.getMessage());
