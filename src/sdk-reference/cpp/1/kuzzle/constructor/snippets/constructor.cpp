a1a4b56ce17b72569082dfc99587dae3a8e0c97f
njuelle <n.juelle@gmail.com>
2018-11-07 15:17:03 +0100

    [test] Fix CPP test: removing options

--- src/sdk-reference/cpp/1/kuzzle/constructor/snippets/constructor.cpp
+++ src/sdk-reference/cpp/1/kuzzle/constructor/snippets/constructor.cpp
@@ -1,4 +1,1 @@
-kuzzleio::options options = KUZZLE_OPTIONS_DEFAULT;
-options.auto_resubscribe = false;
-
-kuzzleio::Kuzzle *kuzzle = new kuzzleio::Kuzzle("kuzzle", &options);
+kuzzleio::Kuzzle *kuzzle = new kuzzleio::Kuzzle("kuzzle");
