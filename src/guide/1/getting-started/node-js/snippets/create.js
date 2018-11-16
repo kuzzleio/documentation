6d6a953beefc53780083f38e62e185d232480df1
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-16 13:50:47 +0100

    [getting-started] add nodejs getting started

---
+++ src/guide/1/getting-started/node-js/snippets/create.js
@@ -0,0 +1,33 @@
+// load the Kuzzle SDK module
+const { Kuzzle } = require('kuzzle-sdk');
+
+// instantiate a Kuzzle client
+const kuzzle = new Kuzzle('websocket', { host: 'localhost' });
+
+// add a listener to detect any connection problems
+kuzzle.on('networkError', error => {
+  console.error(`Network Error: ${error}`);
+});
+
+const run = async () => {
+  try {
+    // Connect to Kuzzle server
+    await kuzzle.connect();
+
+    // Create your document
+    const driver = {
+      name: 'Sirkis',
+      birthday: '1959-06-22',
+      license: 'B'
+    };
+    await kuzzle.document.create('nyc-open-data', 'yellow-taxi', null, driver)
+
+    console.log('New document successfully created!');
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    kuzzle.disconnect();
+  }
+}
+
+run();
