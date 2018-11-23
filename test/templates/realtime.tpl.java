89700bfa8cd8e91630bb1389e9d08c20c73a1715
Aschen (Adrien Maret) <amaret93@gmail.com>
2018-11-23 14:40:06 +0100

    [realtime tests] use retry in java template

--- test/templates/realtime.tpl.java
+++ test/templates/realtime.tpl.java
@@ -1,5 +1,7 @@
 import io.kuzzle.sdk.*;
 import java.util.Date;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class CodeExampleGenericClass {
     private static Kuzzle kuzzle;
@@ -7,11 +9,25 @@ public class CodeExampleGenericClass {
     public static void main(String[] args) {
       kuzzle = new Kuzzle("kuzzle");
       kuzzle.connect();
+
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      PrintStream ps = new PrintStream(baos);
+      PrintStream cout_original = System.out;
+
+      System.setOut(ps);
+
       [snippet-code]
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e) {
-        System.err.println(e.getMessage());
+
+      while (baos.size() == 0) {
+        try {
+          Thread.sleep(200);
+        } catch (InterruptedException e) {
+          System.err.println(e.getMessage());
+        }
       }
+
+      System.out.flush();
+      System.setOut(cout_original);
+      System.out.println(baos.toString());
     }
 }
