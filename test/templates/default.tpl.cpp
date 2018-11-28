851e07f40c081f020590192352f972e30560eefb
scottinet <seb.cottinet@gmail.com>
2018-11-28 10:33:32 +0100

    [c++] update templates to the latest SDK version

--- test/templates/default.tpl.cpp
+++ test/templates/default.tpl.cpp
@@ -1,26 +1,21 @@
 #include <unistd.h>
-
 #include <iostream>
 #include <string>
 #include <ctime>
 #include <vector>
 
-#include "auth.hpp"
-#include "collection.hpp"
-#include "document.hpp"
-#include "server.hpp"
-#include "index.hpp"
-#include "realtime.hpp"
 #include "kuzzle.hpp"
+#include "websocket.hpp"
 
 int main() {
   std::string hostname = "kuzzle";
 
-  kuzzleio::Kuzzle* kuzzle = new kuzzleio::Kuzzle(hostname);
+  kuzzleio::WebSocket* ws = new kuzzleio::WebSocket(hostname);
+  kuzzleio::Kuzzle* kuzzle = new kuzzleio::Kuzzle(ws);
 
   char* error = kuzzle->connect();
   if (error != nullptr) {
-    std::cerr << "Unable to connect to " << hostname << ":7512\n" << error << std::endl;
+    std::cerr << "Unable to connect to " << hostname << ":" << kuzzle->getProtocol()->getPort() << std::endl << error << std::endl;
     return 1;
   }
 
