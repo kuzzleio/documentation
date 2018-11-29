f8f8e200245f40b2b2109996bfe15e6984f4768d
scottinet <seb.cottinet@gmail.com>
2018-11-29 14:46:22 +0100

    [realtime] stabilize realtime tests

--- src/sdk-reference/cpp/1/realtime/subscribe/snippets/user-notifications.cpp
+++ src/sdk-reference/cpp/1/realtime/subscribe/snippets/user-notifications.cpp
@@ -1,6 +1,6 @@
 kuzzleio::NotificationListener listener =
   [](const kuzzleio::notification_result *notification) {
-    std::cout << notification->volatiles << std::endl;
+    std::cout << notification->volatile << std::endl;
     // "{ "username": "nina vkote" }"
     std::cout << "Currently " << notification->result->count << " users in the room" << std::endl;
   };
@@ -18,18 +18,23 @@ try {
     &listener,
     &options);
 
-  // instantiate a second kuzzle client because
-  // the same sdk instance does not receive his own notifications
-  kuzzleio::options fuzzle_options;
-  kuzzleio::WebSocket* another_socket = new kuzzleio::WebSocket("kuzzle");
-  kuzzleio::Kuzzle* fuzzle = new kuzzleio::Kuzzle(another_socket, &fuzzle_options);
-  fuzzle->connect();
+  // Instantiates a second kuzzle client: multiple subscriptions
+  // made by the same user will not trigger "new user" notifications
+  kuzzleio::WebSocket* ws2 = new kuzzleio::WebSocket("kuzzle");
+  kuzzleio::Kuzzle* kuzzle2 = new kuzzleio::Kuzzle(ws2);
+  kuzzle2->connect();
 
   // Set some volatile data
-  fuzzle->setVolatile("{ \"username\": \"nina vkote\" }");
+  kuzzleio::room_options options2;
+  options2.volatiles = "{ \"username\": \"nina vkote\" }";
 
   // Subscribe to the same room with the second client
-  fuzzle->realtime->subscribe("nyc-open-data", "yellow-taxi", filters, &listener);
+  kuzzle2->realtime->subscribe(
+    "nyc-open-data",
+    "yellow-taxi",
+    filters,
+    &listener,
+    &options2);
 } catch (kuzzleio::KuzzleException &e) {
   std::cerr << e.getMessage() << std::endl;
 }
