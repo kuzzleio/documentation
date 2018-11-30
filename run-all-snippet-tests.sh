4a9eefaa5240db9c59f6238a9c7fe5dcad7f2987
Adrien Maret <amaret93@gmail.com>
2018-11-30 17:32:05 +0100

    KZL-757 - Update test framework to test any part of the doc (#152)
    Add the ability for the test framework to test any part of the documentation.
    
    Now each test file .test.yml must provide two additional keys: sdk and version.
    You must provide theses information when you run the tests, the framework will look for every test file that match the sdk name and version.
    
    Exemple: bash run-snippet-tests.sh -n -p src/sdk-reference -s java -v 1

--- run-all-snippet-tests.sh
+++ run-all-snippet-tests.sh
@@ -2,25 +2,31 @@
 
 sh .ci/start_kuzzle.sh
 
-# List all sdk path
-sdk_paths=$(find src/sdk-reference -maxdepth 2 -type d | grep 'src/sdk-reference/[a-z]\{1,\}/[0-9]')
+ERROR=0
 
-error=0
-for sdk_path in $sdk_paths; do
+function start_sdk_tests() {
+    sdk=$1
+    version=$2
+
   echo ""
   echo "#######################################################################"
-  echo "                Test $sdk_path snippets"
+  echo "                Test SDK $sdk $version snippets"
   echo "#######################################################################"
 
-  bash run-snippet-tests.sh -n -p $sdk_path
+  bash run-snippet-tests.sh -n -p src/ -s $sdk -v $version
 
   if [[ ! $? -eq 0 ]]; then
-    error=1
+    ERROR=1
   fi
 
   curl -s -o /dev/null -X POST localhost:7512/admin/_resetKuzzleData
-  curl -s -o /dev/null -X POST localhost:7512/admin/_resetDatabase
+  curl -s -o /dev/null -X POST localhost:7512/admin/_resetDatabase    
+}
 
-done
+start_sdk_tests js 5
+start_sdk_tests js 6
+start_sdk_tests go 1
+start_sdk_tests cpp 1
+start_sdk_tests java 1
 
 exit $error
