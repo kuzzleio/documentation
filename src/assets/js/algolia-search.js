339a94b0e63cdcacecc7a1512d3859bc77b3d667
njuelle <n.juelle@gmail.com>
2018-12-06 17:07:58 +0100

    [JS] ES6 & fix sort function for chrome support

--- src/assets/js/algolia-search.js
+++ src/assets/js/algolia-search.js
@@ -1,39 +1,35 @@
-$(document).ready(function() {
-  algoliaSearch.init();
-});
-
-var algoliaSearch = {
+let algoliaSearch = {
 
   currentTags: window.location.pathname
     .substring(1)
     .split('/')
     .slice(0, 4)
-    .map(function(tag) {
+    .map(tag => {
       if (tag === 'sdk-reference') { return 'sdk'; }
       if (/^[0-9]+$/.test(tag)) { return tag + '.x'; }
       return tag;
     }),
 
-  init: function() {
-    var self = this;
-    var client = algoliasearch(algolia_projectId, algolia_publicKey);
-    var index = client.initIndex(algolia_index);
-    var searchBar = $('.md-search__input');
-    var searchTrigger = $('#search');
-    var resultList = $('.md-search-result__list');
-    var resetButton = $('button.md-icon.md-search__icon');
+  init: () => {
+    const 
+      client = algoliasearch(algolia_projectId, algolia_publicKey),
+      index = client.initIndex(algolia_index),
+      searchBar = $('.md-search__input'),
+      searchTrigger = $('#search'),
+      resultList = $('.md-search-result__list'),
+      resetButton = $('button.md-icon.md-search__icon');
 
     searchBar.on('input', function() {
-      var query = $(this).val();
+      let query = $(this).val();
       if (query.length < 3) {
         resultList.html('');
         return;
       }
-      index.search(query, function(err, content) {
+      index.search(query, (err, content) => {
         if (err) {
           return;
         }
-        self.setResults(content.hits.sort(algoliaSearch.sortByTags), resultList);
+        algoliaSearch.setResults(content.hits.sort(algoliaSearch.sortByTags), resultList);
       });
     });
 
@@ -49,41 +45,44 @@ var algoliaSearch = {
     });
   },
 
-  setResults: function(hits, container) {
-    var content = '';
-    for (var k in hits) {
-      var teaser = hits[k]._highlightResult.content.value;
+  setResults: (hits, container) => {
+    let content = '';
+    for (const hit of Object.values(hits)) {
       content +=
-        '<li class="md-search-result__item">' +
-          '<a href="' + site_url + hits[k].path + '" class="md-search-result__link" title="' + hits[k].title + '">' +
-            '<article class="md-search-result__article">' +
-              '<h1 class="md-search-result__title">' + 
-                hits[k].title + algoliaSearch.formatTags(hits[k].tags) +
-              '</h1>' +
-              '<p class="md-search-result__teaser">' + teaser + '</p>' +
-            '</article>' +
-          '</a>' +
-        '</li>';
+        `<li class="md-search-result__item">
+          <a href="${site_url + hit.path}" class="md-search-result__link" title="${hit.title}">
+            <article class="md-search-result__article">
+              <h1 class="md-search-result__title"> 
+                ${hit.title + algoliaSearch.formatTags(hit.tags)}
+              </h1>
+              <p class="md-search-result__teaser">${hit._highlightResult.content.value}</p>
+            </article>
+          </a>
+        </li>`;
     }
     container.html(content);
   },
 
-  formatTags: function(tags) {
-    var formated = '';
-    for (var k in tags) {
-      formated += '<span class="tag">' + tags[k] + '</span>';
+  formatTags: tags => {
+    let formated = '';
+    for (const tag of Object.values(tags)) {
+      formated += `<span class="tag">${tag}</span>`;
     }
     return formated;
   },
   
-  sortByTags: function(a, b) {
-    return algoliaSearch.getTagsScore(a.tags) < algoliaSearch.getTagsScore(b.tags);
+  sortByTags: (a, b) => {
+    const 
+      scoreA = algoliaSearch.getTagsScore(a.tags),
+      scoreB = algoliaSearch.getTagsScore(b.tags);
+
+    return Math.sign(scoreB - scoreA);
   },
-  
-  getTagsScore: function(tags) {
-    var score = 0;
-    for(var k in tags) {
-      if (algoliaSearch.currentTags.includes(tags[k])) {
+
+  getTagsScore: (tags) => {
+    let score = 0;
+    for (const tag of Object.values(tags)) {
+      if (algoliaSearch.currentTags.includes(tag)) {
         ++score;
       }
     }
@@ -91,3 +90,7 @@ var algoliaSearch = {
   }
 
 };
+
+$(document).ready(function() {
+  algoliaSearch.init();
+});
