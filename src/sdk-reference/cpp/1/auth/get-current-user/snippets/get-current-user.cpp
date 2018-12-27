6a972055d8bf1cd03212e42158b50c5b729b0cd7
Eric <31733541+etrousset@users.noreply.github.com>
2018-12-27 14:52:05 +0100

    Update src/sdk-reference/cpp/1/auth/get-current-user/snippets/get-current-user.cpp
    Co-Authored-By: Aschen <amaret93@gmail.com>

--- src/sdk-reference/cpp/1/auth/get-current-user/snippets/get-current-user.cpp
+++ src/sdk-reference/cpp/1/auth/get-current-user/snippets/get-current-user.cpp
@@ -2,7 +2,7 @@ try {
   kuzzle->auth->login("local", R"({"username":"foo","password":"bar"})");
   kuzzleio::User user = kuzzle->auth->getCurrentUser();
 
-  std::cout << "Successfully get current user" << std::endl;
+  std::cout << "Successfully got current user" << std::endl;
 } catch (kuzzleio::KuzzleException &e) {
   std::cerr << e.what() << std::endl;
 }
