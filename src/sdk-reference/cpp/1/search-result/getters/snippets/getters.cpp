3121cd7734363d466c5b3116e773e0e008d2cc4b
Aschen (Adrien Maret) <amaret93@gmail.com>
2019-01-06 11:58:28 +0100

    Add getters to SearchResult

---
+++ src/sdk-reference/cpp/1/search-result/getters/snippets/getters.cpp
@@ -0,0 +1,21 @@
+try {
+  kuzzleio::SearchResult* results = kuzzle->document->search(
+    "nyc-open-data",
+    "yellow-taxi",
+    R"({
+      "query": {
+        "match": {
+          "category": "suv"
+        }
+      }
+    })",
+    options);
+
+  size_t total_documents = results->total();
+  size_t fetched_documents = results->fetched();
+  std::string aggregations = results->aggregations();
+  std::string hits = results->hits();
+  std::string scroll_id = results->scroll_id();
+} catch (kuzzleio::KuzzleException& e) {
+  std::cerr << e.what() << std::endl;
+}
