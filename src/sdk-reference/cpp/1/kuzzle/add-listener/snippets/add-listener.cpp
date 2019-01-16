ba71c324b5d9a6963d9c2d0ae16155cd6c66c154
Eric Trousset <etrousset@kaliop.com>
2019-01-16 10:25:08 +0100

    [test cpp]: fixed cpp test after SDK has changed

--- src/sdk-reference/cpp/1/kuzzle/add-listener/snippets/add-listener.cpp
+++ src/sdk-reference/cpp/1/kuzzle/add-listener/snippets/add-listener.cpp
@@ -5,6 +5,6 @@ kuzzleio::EventListener other_listener = [](const std::string &payload) {
   std::cerr << payload << std::endl;
 };
 
-kuzzle->addListener(CONNECTED, &listener)->addListener(CONNECTED, &other_listener);
+kuzzle->addListener(EVENT_CONNECTED, &listener)->addListener(EVENT_CONNECTED, &other_listener);
 
 std::cout << "Listener successfully added" << std::endl;
