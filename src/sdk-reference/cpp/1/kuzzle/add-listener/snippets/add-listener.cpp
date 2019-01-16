46380429ec8f32542abb9731ebb8aa1f4cc946de
Eric Trousset <etrousset@kaliop.com>
2019-01-16 11:49:30 +0100

    test & doc: more snippet fixing, and documentation update

--- src/sdk-reference/cpp/1/kuzzle/add-listener/snippets/add-listener.cpp
+++ src/sdk-reference/cpp/1/kuzzle/add-listener/snippets/add-listener.cpp
@@ -5,6 +5,6 @@ kuzzleio::EventListener other_listener = [](const std::string &payload) {
   std::cerr << payload << std::endl;
 };
 
-kuzzle->addListener(EVENT_CONNECTED, &listener)->addListener(EVENT_CONNECTED, &other_listener);
+kuzzle->addListener(KUZZLE_EVENT_CONNECTED, &listener)->addListener(KUZZLE_EVENT_CONNECTED, &other_listener);
 
 std::cout << "Listener successfully added" << std::endl;
