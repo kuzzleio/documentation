ad36c485e15a0ae6708a6da769f72b1a4a6116f4
scottinet <seb.cottinet@gmail.com>
2019-02-13 17:14:51 +0100

    [cpp] update kuzzle:addListener snippet

--- src/sdk-reference/cpp/1/kuzzle/add-listener/snippets/add-listener.cpp
+++ src/sdk-reference/cpp/1/kuzzle/add-listener/snippets/add-listener.cpp
@@ -1,10 +1,14 @@
-kuzzleio::EventListener listener = [](const std::string &payload) {
-  std::cout << payload << std::endl;
-};
-kuzzleio::EventListener other_listener = [](const std::string &payload) {
-  std::cerr << payload << std::endl;
-};
+kuzzleio::SharedEventListener listener = std::make_shared<EventListener>(
+  [](const std::string &payload) {
+    std::cout << payload << std::endl;
+  });
+kuzzleio::SharedEventListener other_listener = std::make_shared<EventListener>(
+  [](const std::string &payload) {
+    std::cerr << payload << std::endl;
+  });
 
-kuzzle->addListener(KUZZLE_EVENT_CONNECTED, &listener)->addListener(KUZZLE_EVENT_CONNECTED, &other_listener);
+kuzzle
+  ->addListener(KUZZLE_EVENT_CONNECTED, listener)
+  ->addListener(KUZZLE_EVENT_CONNECTED, other_listener);
 
 std::cout << "Listener successfully added" << std::endl;
