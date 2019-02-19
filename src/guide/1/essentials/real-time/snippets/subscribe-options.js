284659e264a105e0aaeb699c6ca6d4ee24a7ffb5
Yoann Abbes <yoann.abbes@gmail.com>
2019-02-19 17:51:03 +0100

    add snippets js sdk6 to real-time notification

---
+++ src/guide/1/essentials/real-time/snippets/subscribe-options.js
@@ -0,0 +1,23 @@
+// Create a filter that defines a regexp
+const filter = {regexp: {label: 'URGENT'}};
+// The Options object
+const options = {subscribeToSelf: false};
+// Triggered whenever a document matching the filter is submitted to Kuzzle
+const callback = (error, notification) => {
+  if (error) {
+    throw new Error(error);
+  }
+  console.log('Something happened and we should do something URGENTLY.', notification);
+};
+try {
+  await kuzzle.realtime.subscribe(
+    'toto-list',
+    'todos',
+    filter,
+    callback,
+    options
+  );
+  console.log('subscribe ok');
+} catch (error) {
+  console.error(error.message);
+}
