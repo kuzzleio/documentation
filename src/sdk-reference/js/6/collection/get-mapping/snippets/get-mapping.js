b387aa144c890dcc4c0fe3b1487d5c4f1cfb1ff9
Eric Trousset <etrousset@kaliop.com>
2019-04-11 17:16:01 +0200

    doc: disambiguation of getMapping sample response in JS SDK

--- src/sdk-reference/js/6/collection/get-mapping/snippets/get-mapping.js
+++ src/sdk-reference/js/6/collection/get-mapping/snippets/get-mapping.js
@@ -1,21 +1,38 @@
 try {
   const mapping = await kuzzle.collection.getMapping('nyc-open-data', 'yellow-taxi');
   console.log(mapping);
+
   /*
-    {
-      properties: {
-        license: { type: 'keyword' },
-        driver: {
-          properties: {
-            name: { type: 'keyword' },
-            curriculum: { type: 'text' }
+  {
+    "nyc-open-data": {
+      "mappings": {
+        "yellow-taxi": {
+          "dynamic": "false",
+          "_meta": {
+            "area": "Panipokhari"
+          },
+          "properties": {
+            "license": {
+              "type": "keyword"
+            },
+            "driver": {
+              "properties": {
+                "name": {
+                  "type": "keyword"
+                },
+                "curriculum": {
+                  "type": "text"
+                }
+              }
+            }
           }
         }
       }
     }
+  }
   */
 
-  console.log('Success');
+  console.log('uccess');
 } catch (error) {
   console.error(error.message);
 }
