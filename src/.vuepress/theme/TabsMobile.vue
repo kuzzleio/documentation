02d74d9ecf7cd03707af9e391317b05e962c1cbc
Luca Marchesini <xbill82@gmail.com>
2019-06-11 10:16:41 +0200

    Fixed tabs-mobile

--- src/.vuepress/theme/TabsMobile.vue
+++ src/.vuepress/theme/TabsMobile.vue
@@ -4,45 +4,51 @@
       <SDKSelector :items="sdkList"/>
     </div>
 
-    <a
-      href="/guide"
+    <router-link
+      :to="{path: generateLink('/core/1/guides/')}"
       title="Guides"
       class="md-source"
       data-md-state="done"
       style="display:inline-block;"
     >
       <div class="md-source__repository">Guides</div>
-    </a>
-    <a
-      href="/sdk-reference"
+    </router-link>
+    <router-link
+      :to="{path:generateLink('/core/1/api/')}"
+      title="API"
+      class="md-source"
+      data-md-state="done"
+      style="display:inline-block;"
+    >
+      <div class="md-source__repository">API</div>
+    </router-link>
+    <router-link
+      :to="{path: '/sdk/'}"
       title="SDKs"
       class="md-source"
       data-md-state="done"
       style="display:inline-block;"
     >
       <div class="md-source__repository">SDK</div>
-    </a>
-    <a href="/api" title="API" class="md-source" data-md-state="done" style="display:inline-block;">
-      <div class="md-source__repository">API</div>
-    </a>
-    <a
-      href="/plugins"
+    </router-link>
+    <router-link
+      :to="{path: generateLink('/core/1/plugins/')}"
       title="Plugins"
       class="md-source"
       data-md-state="done"
       style="display:inline-block;"
     >
       <div class="md-source__repository">Plugins</div>
-    </a>
-    <a
-      href="/protocols"
+    </router-link>
+    <router-link
+      :to="{path: generateLink('/core/1/protocols/')}"
       title="Protocols"
       class="md-source"
       data-md-state="done"
       style="display:inline-block;"
     >
       <div class="md-source__repository">Protocols</div>
-    </a>
+    </router-link>
   </div>
 </template>
 
