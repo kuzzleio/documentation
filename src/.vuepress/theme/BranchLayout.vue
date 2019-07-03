7bd7d6046f1c1686b13035069674dd1d7d84e3b4
Luca Marchesini <xbill82@gmail.com>
2019-07-03 15:35:06 +0200

    [opt] Simplified layout for branch and root nodes

---
+++ src/.vuepress/theme/BranchLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+const { getFirstValidChild } = require('../util.js');
+
+export default {
+  mounted() {
+    if (this.$page.frontmatter.type !== 'page') {
+      this.$router.replace(getFirstValidChild(this.$page, this.$site.pages));
+    }
+  }
+};
+</script>
+
+<style>
+</style>
