4f62364f2aa19a299af2410f7d9e27e62f031817
Aschen <amaret93@gmail.com>
2019-07-22 12:00:55 +0200

    use git clone instead submodules

--- .ci/stop_kuzzle.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f .ci/docker-compose.yml down
--- .repos/clone-repos.rb
+++ .repos/clone-repos.rb
@@ -1,14 +1,18 @@
 require 'yaml'
 
-repositories = YAML.load_file('./repositories.yml')
+repositories = YAML.load_file("#{__dir__}/repositories.yml")
 
 repositories.each do |repository|
-  cmd = `git clone \
-    --depth 10
-    --single-branch \
-    --branch #{repository.branch} \
-    #{repository.url} \
-    #{repository.destination}`
+  cmd = """
+git clone \
+--depth 10 \
+--single-branch \
+--branch #{repository['branch']} \
+#{repository['url']} \
+#{__dir__}/#{repository['destination']}
+  """
 
   puts cmd
+
+  `#{cmd}`
 end
