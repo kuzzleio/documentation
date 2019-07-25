bb0ee0c01cd13ee8b2f457ae09f607c9d9e09d75
Aschen <amaret93@gmail.com>
2019-07-25 16:18:40 +0200

    add build of repositories

---
+++ .repos/colorOutput.js
@@ -0,0 +1,43 @@
+const clc = require('cli-color');
+
+function noop(str) {
+  return str;
+}
+
+class ColorOutput {
+  constructor () {
+
+    this.format = {
+      error: clc.red,
+      warn: clc.yellow,
+      notice: clc.cyanBright,
+      ok: clc.green.bold,
+      question: clc.whiteBright
+    };
+  }
+
+  /* eslint-disable no-console */
+  error(str) {
+    console.error(this.format.error(`[✖] ${str}`));
+  }
+
+  warn(str) {
+    console.warn(this.format.warn(`[ℹ] ${str}`));
+  }
+
+  notice(str) {
+    console.log(this.format.notice(`[ℹ] ${str}`));
+  }
+
+  question(str) {
+    console.log(this.format.question(str));
+  }
+
+  ok(str) {
+    console.log(this.format.ok(`[✔] ${str}`));
+  }
+
+  /* eslint-enable no-console */
+}
+
+module.exports = new ColorOutput();
---
+++ .repos/repositories.js
@@ -0,0 +1,144 @@
+const
+  cout = require('./colorOutput'),
+  { exec } = require('child_process'),
+  fs = require('fs'),
+  YAML = require('js-yaml');
+
+const currentDir = require('path').dirname(require.main.filename);
+
+const getRepositories = (argv) => {
+  let repositoryNames = [];
+
+  if (process.env.REPOSITORIES) {
+    repositoryNames = process.env.REPOSITORIES.split(',');
+  } else if (argv.repo) {
+    repositoryNames = argv.repo;
+  }
+
+  const repositories = YAML
+    .safeLoad(fs.readFileSync(`${currentDir}/repositories.yml`))
+    .filter(repo => repositoryNames.length === 0 || repositoryNames.includes(repo.name));
+
+  if (repositories.length === 0) {
+    cout.error(`Unknown repository ${repositoryName}.`);
+    process.exit(1);
+  }
+
+  return repositories;
+};
+
+const execute = (command, message) => {
+  const cmd = Array.isArray(command)
+    ? command.join(' ')
+    : command;
+
+  cout.notice(message);
+
+  return new Promise(resolve => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(stderr);
+        console.error(stdout);
+        cout.error(cmd);
+        return resolve(error);
+      }
+
+      cout.ok(message);
+      resolve();
+    })
+  })
+}
+
+const cloneRepository = async (argv) => {
+  const
+    branch = argv.branch || 'stable',
+    promises = [];
+
+  if (!['dev', 'stable'].includes(branch)) {
+    cout.warn('--branch option can be "dev" or "stable"');
+    process.exit(1);
+  }
+
+  for (const repository of getRepositories(argv)) {
+    const
+      message = `Cloning ${repository.name} on branch ${repository[branch]}`,
+      command = [
+        'git clone',
+        '--depth 10',
+        '--single-branch',
+        `--branch ${repository[branch]}`,
+        repository.url,
+        `${currentDir}/${repository.destination}`
+      ];
+
+    promises.push(execute(command, message));
+  }
+
+  await Promise.all(promises);
+};
+
+const commandRepository = async (cmd, argv) => {
+  const promises = [];
+
+  for (const repository of getRepositories(argv)) {
+    const
+      message = `Executing command "${cmd}" for ${repository.name}`,
+      command = `cd ${currentDir}/${repository.destination} && ${cmd}`;
+
+    const promise = execute(command, message);
+
+    if (argv.async) {
+      promises.push(promise);
+    } else {
+      await promise;
+    }
+  }
+
+  await Promise.all(promises);
+};
+
+const updateRepository = async (argv) => {
+  await commandRepository('git pull', argv);
+};
+
+
+const removeRepository = async (argv) => {
+  const promises = [];
+
+  for (const repository of getRepositories(argv)) {
+    const
+      message = `Remove repository ${repository.name}`,
+      command = `rm -rf ${currentDir}/${repository.destination}`;
+
+    promises.push(execute(command, message));
+  }
+
+  await Promise.all(promises);
+}
+
+const argv = require('yargs').argv;
+
+switch (process.argv[2]) {
+  case 'run':
+    const command = process.argv[3];
+
+    commandRepository(command, argv)
+    break;
+
+  case 'clone':
+    cloneRepository(argv);
+    break;
+
+  case 'update':
+    updateRepository(argv);
+    break;
+
+  case 'remove':
+    removeRepository(argv);
+    break;
+
+  default:
+    cout.error(`Invalid command "${process.argv[2]}". Use "clone", "run", "update" or "remove".`);
+    process.exit(1);
+    break;
+}
