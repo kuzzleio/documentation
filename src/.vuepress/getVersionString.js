6f0cb541609e2634e39d293c07419cba5592592a
Aschen <amaret93@gmail.com>
2019-07-29 14:06:24 +0200

    add date to version string

--- src/.vuepress/getVersionString.js
+++ src/.vuepress/getVersionString.js
@@ -4,12 +4,6 @@ const
 
 const currentDir = __dirname;
 
-let docDir = path.resolve(`${currentDir}/..`);
-// This variable is set if we are inside a sub-site build
-if (process.env.DOC_DIR) {
-  docDir = path.resolve(`${currentDir}/../../../${process.env.DOC_DIR}`);
-}
-
 let repoDir = path.resolve(`${currentDir}/../..`);
 // This variable is set if we are inside a sub-site build
 if (process.env.DOC_DIR) {
@@ -19,7 +13,8 @@ if (process.env.DOC_DIR) {
 const package = require(`${repoDir}/package.json`);
 const repository = package.repository ? package.repository.url : package.name;
 const commit = execSync(`cd ${repoDir} && git rev-parse HEAD`, { encoding: 'utf8' }).replace('\n', '');
+const date = (new Date()).toISOString();
 
-const versionString = `${repository}@${commit}`;
+const versionString = `${repository}@${commit}@${date}`;
 
 module.exports = versionString;
