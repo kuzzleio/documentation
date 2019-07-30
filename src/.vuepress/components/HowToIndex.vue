e0bbf3f7804206f99f9493c1c98d7b80b546770d
Berthier <Berthier.est@gmail.com>
2019-07-30 11:48:57 +0200

    add logos and tab and symlink

---
+++ src/.vuepress/components/HowToIndex.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Tiles">
+    <a href="/how-to/doc/keep-only-warm-data" class="Tiles-item">
+      <img src="/logos/logo-warm-data.svg" alt="warm data logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Keep warm data</div>
+    </a>
+    <a href="/how-to/doc/kuzzle-esp32" class="Tiles-item">
+      <img src="/logos/logo-espressif.svg" alt="espressif logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">IOT backend ESP32 </div>
+    </a>
+        <a href="/how-to/doc/massive-data-import" class="Tiles-item">
+      <img src="/logos/logo-data.svg" alt="data logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Massive data import</div>
+    </a>
+    <a href="/how-to/doc/monitor-iot-data-with-freeboard" class="Tiles-item">
+      <img src="/logos/logo-dashboard.svg" alt="dashboard logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Monitor IOT data</div>
+    </a>
+    <a href="/how-to/doc/sync-data-to-another-databse" class="Tiles-item">
+      <img src="/logos/logo-database.svg" alt="database logo" class="Tiles-item-logo">
+      <div class="Tiles-item-name">Sync to other DB</div>
+    </a>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
