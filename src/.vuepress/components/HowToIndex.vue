0f24e942f7192af0321bbc4a96807d8dec35b0e3
Berthier <Berthier.est@gmail.com>
2019-07-31 09:08:38 +0200

    ignore kuzzle-how-to and set master branch and set url how-to/1

--- src/.vuepress/components/HowToIndex.vue
+++ src/.vuepress/components/HowToIndex.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="Tiles">
-    <a href="/how-to/doc/keep-only-warm-data" class="Tiles-item">
+    <a href="/how-to/1/keep-only-warm-data" class="Tiles-item">
       <img src="/logos/logo-warm-data.svg" alt="warm data logo" class="Tiles-item-logo">
       <div class="Tiles-item-name">Keep warm data</div>
     </a>
-    <a href="/how-to/doc/kuzzle-esp32" class="Tiles-item">
+    <a href="/how-to/1/kuzzle-esp32" class="Tiles-item">
       <img src="/logos/logo-espressif.svg" alt="espressif logo" class="Tiles-item-logo">
       <div class="Tiles-item-name">IOT backend ESP32 </div>
     </a>
-        <a href="/how-to/doc/massive-data-import" class="Tiles-item">
+        <a href="/how-to/1/massive-data-import" class="Tiles-item">
       <img src="/logos/logo-data.svg" alt="data logo" class="Tiles-item-logo">
       <div class="Tiles-item-name">Massive data import</div>
     </a>
-    <a href="/how-to/doc/monitor-iot-data-with-freeboard" class="Tiles-item">
+    <a href="/how-to/1/monitor-iot-data-with-freeboard" class="Tiles-item">
       <img src="/logos/logo-dashboard.svg" alt="dashboard logo" class="Tiles-item-logo">
       <div class="Tiles-item-name">Monitor IOT data</div>
     </a>
-    <a href="/how-to/doc/sync-data-to-another-databse" class="Tiles-item">
+    <a href="/how-to/1/sync-data-to-another-database" class="Tiles-item">
       <img src="/logos/logo-database.svg" alt="database logo" class="Tiles-item-logo">
       <div class="Tiles-item-name">Sync to other DB</div>
     </a>
