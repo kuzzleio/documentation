c6088bee4286081191e6b4590ee6dc8ba3373123
Berthier <Berthier.est@gmail.com>
2019-09-03 17:24:03 +0200

    fix sidebar

--- src/.vuepress/theme/Sidebar.vue
+++ src/.vuepress/theme/Sidebar.vue
@@ -26,8 +26,9 @@
                     class="md-nav__link"
                     :class="{'md-nav__link--active': $page.path === item__2.path, 'md-nav__item--code': item__2.frontmatter.code == true}"
                   >
-                    <div v-if="getPageChildren(item__2).length"
-                      @click="openOrCloseOrRedirect(item__1, item__2)"
+                    <div
+                      v-if="getPageChildren(item__2).length"
+                      @click="handleSubmenuClick(item__1, item__2)"
                     >
                       <i
                         v-if="openedSubmenu === getId([item__1.title, item__2.title])"
@@ -37,10 +38,7 @@
                       <i v-else class="fa fa-caret-right" aria-hidden="true"></i>
                       <span>{{item__2.title}}</span>
                     </div>
-                    <router-link v-else
-                      :to="item__2.path"
-                      @click.native="closeSidebar"
-                    >
+                    <router-link v-else :to="item__2.path" @click.native="closeSidebar">
                       <a class="no_arrow">{{item__2.title}}</a>
                     </router-link>
                   </div>
@@ -89,16 +87,16 @@
 </template>
 
 <script>
-import TabsMobile from './TabsMobile.vue';
+import TabsMobile from "./TabsMobile.vue";
 import {
   getPageChildren,
-  getFirstValidChild,
   findRootNode,
   setItemLocalStorage,
   getItemLocalStorage,
+  getNodeByPath,
   getOldSDK
-} from '../util.js';
-import sdkList from '../sdk.json';
+} from "../util.js";
+import sdkList from "../sdk.json";
 
 export default {
   components: {
@@ -112,7 +110,7 @@ export default {
   },
   data() {
     return {
-      openedSubmenu: '',
+      openedSubmenu: "",
       sdkList
     };
   },
@@ -129,20 +127,24 @@ export default {
   },
   methods: {
     setOpenedSubmenu(item__1, item__2) {
-      setItemLocalStorage('item__1', item__1);
-      setItemLocalStorage('item__2', item__2);
+      setItemLocalStorage("item__1", item__1);
+      setItemLocalStorage("item__2", item__2);
       this.openedSubmenu = this.getId([item__1.title, item__2.title]);
     },
     unsetOpenedSubmenu() {
-      localStorage.setItem('item__1', null);
-      localStorage.setItem('item__2', null);
-      this.openedSubmenu = '';
+      localStorage.setItem("item__1", null);
+      localStorage.setItem("item__2", null);
+      this.openedSubmenu = "";
     },
     closeSubmenu() {
-      if (this.openedSubmenu !== '') {
-        const openedSubmenuId = this.sanitize(this.openedSubmenu);
-        document.getElementById(openedSubmenuId).style.height = '0px';
+      if (this.openedSubmenu === "") {
+        return;
       }
+      const openedSubmenuId = this.sanitize(this.openedSubmenu);
+      if (!document.getElementById(openedSubmenuId)) {
+        return;
+      }
+      document.getElementById(openedSubmenuId).style.height = "0px";
     },
     redirect(item__2) {
       this.closeSidebar();
@@ -151,35 +153,40 @@ export default {
     },
     openSubmenu(item__1, item__2) {
       const childs = this.getPageChildren(item__2);
+      if (!childs) {
+        return;
+      }
       const item2Id = this.getId([item__1.title, item__2.title]);
       const item3Id = this.getId([
         item__1.title,
         item__2.title,
         childs[0].title
       ]);
-
+      if (!document.getElementById(item3Id)) {
+        return;
+      }
       const childSize = document.getElementById(item3Id).offsetHeight;
       const menuHeight = `${childs.length * childSize}px`;
       document.getElementById(item2Id).style.height = menuHeight;
     },
     closeSidebar(item) {
-      this.$emit('closeSidebar');
+      this.$emit("closeSidebar");
     },
     subMenuClass(item__1, item__2) {
       return this.openedSubmenu === this.getId([item__1.title, item__2.title])
-        ? 'displaySubmenu'
-        : '';
+        ? "displaySubmenu"
+        : "";
     },
     getId(itemsTitle) {
       return itemsTitle.reduce(
-        (id, item) => id + '_' + this.sanitize(item),
-        ''
+        (id, item) => id + "_" + this.sanitize(item),
+        ""
       );
     },
     sanitize(str) {
-      return str.replace(/ /g, '_');
+      return str.replace(/ /g, "_");
     },
-    openOrCloseOrRedirect(item__1, item__2) {
+    handleSubmenuClick(item__1, item__2) {
       const childs = this.getPageChildren(item__2);
       const clickedSubmenuId = this.getId([item__1.title, item__2.title]);
 
@@ -198,9 +205,6 @@ export default {
     getPageChildren(page) {
       return getPageChildren(page, this.$site.pages);
     },
-    getFirstValidChild(page) {
-      return getFirstValidChild(page, this.$site.pages);
-    },
     /**
      * @param {Element} target
      */
@@ -217,48 +221,23 @@ export default {
     }
   },
   mounted() {
-    let item__1 = getItemLocalStorage('item__1');
-    let item__2 = getItemLocalStorage('item__2');
-
-    if (this.$route.path.match(/\/sdk\//)) {
-      const path = this.$route.path.split('sdk')[1].split('/');
-      const sdk = path[1] + path[2];
-      if (this.oldSDK.includes(sdk)) {
-        return;
-      }
-    }
-
+    let path = this.$route.path;
+    const splitted = path.split("/");
+    const secondPath = path.replace(`${splitted[splitted.length - 2]}/`, "");
+    const thirdPath = secondPath.replace(`${splitted[splitted.length - 3]}/`, "");
+    const node2 = getNodeByPath(secondPath, this.$site.pages);
+    const node3 = getNodeByPath(thirdPath, this.$site.pages);
+    item__2 = node2;
+    item__1 = node3;
     if (!item__1 || !item__2) {
       return;
     }
-
-    // Hack for link sdk/controllers/** to api/api_reference/**
-    if (
-      this.$route.path.match(/\/sdk\//) &&
-      item__1.title === 'API reference'
-    ) {
-      item__1 = this.getPageChildren(this.root).find(
-        el => el.title === 'Controllers'
-      );
-      item__2 = this.getPageChildren(item__1).find(
-        el => el.title === item__2.title
-      );
-      item__2 = item__2 || getItemLocalStorage('item__2');
-    } else if (
-      this.$route.path.match(/\/core\/1\/api\//) &&
-      item__1.title === 'Controllers'
-    ) {
-      item__1 = this.getPageChildren(this.root).find(
-        el => el.title === 'API reference'
-      );
-      item__2 = this.getPageChildren(item__1).find(
-        el => el.title === item__2.title
-      );
-    }
-    this.openOrCloseOrRedirect(item__1, item__2);
+    this.closeSubmenu();
+    this.openSubmenu(item__1, item__2);
+    this.setOpenedSubmenu(item__1, item__2);
     document.onreadystatechange = () => {
-      if (document.readyState === 'complete') {
-        const activeLink = this.$el.querySelector('.md-nav__link--active');
+      if (document.readyState === "complete") {
+        const activeLink = this.$el.querySelector(".md-nav__link--active");
         if (activeLink && !this.isInViewport(activeLink)) {
           const activeDiv = activeLink.parentElement.parentElement;
           const scroll =
