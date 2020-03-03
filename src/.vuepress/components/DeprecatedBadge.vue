8ac37d7bb41ff3872c4b3bc098022cd4b15dc86c
sixla <sixtine.lanord@gmail.com>
2020-03-03 14:07:15 +0100

    [front] Delete DeprecatedBadge.vue modifications

--- src/.vuepress/components/DeprecatedBadge.vue
+++ src/.vuepress/components/DeprecatedBadge.vue
@@ -1,17 +1,9 @@
 <template>
-  <div>
-    <p>Beware, this SDK is deprecated.</p>
-    <Badge
-      type="error"
-      :text="`Deprecated since ${version}`"
-      id="deprecatedBadge"
-    />
-  </div>
+  <Badge type="error" :text="`Deprecated since ${version}`" />
 </template>
 
 <script>
 import Badge from '@vuepress/theme-default/global-components/Badge.vue';
-
 export default {
   components: {
     Badge
@@ -25,9 +17,4 @@ export default {
 };
 </script>
 
-<style>
-#deprecatedBadge {
-  font-size: 22px;
-  vertical-align: middle;
-}
-</style>
+<style></style>
