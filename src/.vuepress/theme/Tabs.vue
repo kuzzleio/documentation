33e2374c9870c939e0d78f3be8c32be35189a5b7
Luca Marchesini <xbill82@gmail.com>
2020-07-07 16:07:28 +0200

    [fix] tab active

--- src/.vuepress/theme/Tabs.vue
+++ src/.vuepress/theme/Tabs.vue
@@ -3,16 +3,21 @@
   <nav class="md-tabs" data-md-component="tabs">
     <div class="md-tabs__inner md-grid">
       <ul class="md-tabs__list">
-        <li class="md-tabs__group" v-for="([part, links]) of headerEntries">
+        <li class="md-tabs__group" v-for="[part, links] of headerEntries">
           <p class="md-tabs__group-name">{{ part }}</p>
           <ul class="md-tabs__group-items">
             <li class="md-tabs__item" v-for="link of links">
               <a
                 :href="link.path"
-                :class="{'md-tabs__link--active': $route.path.match(link.path)}"
+                :class="{
+                  'md-tabs__link--active': `${$site.base}${$route.path}`.match(
+                    link.path
+                  )
+                }"
                 :title="link.label"
                 class="md-tabs__link"
-              >{{ link.label }}</a>
+                >{{ link.label }}</a
+              >
             </li>
           </ul>
         </li>
@@ -22,8 +27,8 @@
 </template>
 
 <script>
-import { getValidLinkByRootPath } from "../util.js";
-import headerEntries from "../header-entries.json";
+import { getValidLinkByRootPath } from '../util.js';
+import headerEntries from '../header-entries.json';
 
 export default {
   props: ['kuzzleMajor'],
