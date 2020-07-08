e001536eac6dc8e6f8fbcabeceff6811693b2c26
Aschen <adrien@maret.email>
2020-07-08 06:04:46 +0200

    Add WarningBanner and bumb vuepress

---
+++ src/.vuepress/components/WarningBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alert alert-warning" id="banner">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WarningBanner'
+};
+</script>
+
+<style>
+#banner {
+  font-size: 18px;
+  vertical-align: middle;
+}
+</style>
