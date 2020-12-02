16b70e5e33946d414647f0c0c2430bb07ec73ab0
njuelle <n.juelle@gmail.com>
2020-12-02 09:27:56 +0100

    add custom badge component

---
+++ src/.vuepress/components/CustomBadge.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Badge :type="type" :text="text" />
+  </div>
+</template>
+
+<script>
+import Badge from '@vuepress/theme-default/global-components/Badge.vue';
+
+export default {
+  components: {
+    Badge
+  },
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true,
+      validator: function(value) {
+        return ['tip', 'error', 'warning'].indexOf(value) !== -1;
+      }
+    }
+  }
+};
+</script>
