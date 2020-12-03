792679828ab72b41a7a299e0ed6721a36f28aa9b
njuelle <n.juelle@gmail.com>
2020-12-03 15:26:40 +0100

    add sdk jvm link on java deprecated banner

--- src/.vuepress/components/JavaDeprecatedBanner.vue
+++ src/.vuepress/components/JavaDeprecatedBanner.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="alert alert-warning deprecatedBanner">
     <p>
-      This SDK is deprecated. We recommend to use the Kuzzle SDK-JVM.<br />
+      This SDK is deprecated. We recommend to use the
+      <a href="/sdk/jvm/1/core-classes/kuzzle/constructor/">Kuzzle SDK-JVM</a
+      >.<br />
       A migration guide is available
 
       <a href="/sdk/java/3/migration/how-to/">here</a>
