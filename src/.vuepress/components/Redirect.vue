79639dffa1e53e4e7c65cd339eb53bde9460b445
rolljee <n.riquelmebareiro@gmail.com>
2025-07-09 11:34:52 +0200

    fix: issue with redirect component

--- src/.vuepress/components/Redirect.vue
+++ src/.vuepress/components/Redirect.vue
@@ -8,38 +8,15 @@ import { useRouter } from 'vuepress/client';
 export default {
   name: 'redirect',
   props: {
-    reload: {
-      type: Boolean,
-      default: false,
-    },
     to: {
       type: String,
       required: true,
     },
   },
-  setup() {
-    return { router$: useRouter() };
-  },
   mounted() {
-    let target = this.to;
-    if (!target.endsWith('/')) {
-      target += '/';
-    }
-
-    if (!this.reload) {
-      this.router$
-        .push(target)
-        .then(() => {
-          console.log(`Redirected to ${target}`);
-        })
-        .catch((err) => {
-          if (err) {
-            // Nothing with the error
-          }
-        });
-    } else {
-      window.location.href = target;
-    }
+    const router = useRouter();
+    const targetUrl = this.to.endsWith('/') ? this.to : `${this.to}/`;
+    router.push(targetUrl);
   },
 };
 </script>
